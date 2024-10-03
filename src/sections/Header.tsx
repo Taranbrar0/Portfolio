edbb9f35ad2fc27fc5ab18eb92bf61b7df72480b
Taranbrar0 <112814527+Taranbrar0@users.noreply.github.com>
2024-10-02 22:17:18 -0700

    fixed header z-index

--- src/sections/Header.tsx
+++ src/sections/Header.tsx
@@ -1,5 +1,5 @@
 export const Header = () => {
-  return <div className="flex justify-center items-center">
+  return <div className="flex justify-center items-center z-20">
     <nav className="flex gap-1 p-0.5 border border-white/15 rounded-full bg-white/10 backdrop-blur fixed top-3 z-10">
       <a href="#" className="nav-item">Home</a>
       <a href="#Projects" className="nav-item">Projects</a>
