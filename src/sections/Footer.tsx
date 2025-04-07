7ffa573f3a2af73ca35cbc1f93b2a13e46417f1a
Taran <112814527+Taranbrar0@users.noreply.github.com>
2025-04-06 18:22:00 -0700

    Update Footer.tsx
    updated the Instagram

--- src/sections/Footer.tsx
+++ src/sections/Footer.tsx
@@ -29,7 +29,7 @@ export const Footer = () => {
             </a>
 
             <a
-              href="https://www.linkedin.com/in/taran-brar/"
+              href="https://www.linkedin.com/in/t4rn.br4r/"
               className="inline-flex items-center gap-1.5 hover:underline"
               target="_blank"
               rel="noopener noreferrer"
